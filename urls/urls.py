6c79c9553530283f4547164cfcd654445eb7c103
Dennis Hedegaard <dennis@dhedegaard.dk>
2013-12-28 19:16:12 +0100

    added favicon

--- urls/urls.py
+++ urls/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^create$', 'app.views.create', name='create'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login',
-    	{'template_name': 'login.html'}, name='login'),
+        {'template_name': 'login.html'}, name='login'),
     url(r'^logout$', 'app.views.logout', name='logout'),
     url(r'^$', 'app.views.list', name='list'),
     url(r'^delete$', 'app.views.delete', name='delete'),
