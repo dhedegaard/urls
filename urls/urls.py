d1bafe2d9f4ec155f5e05e777c2d37c2c2e1160c
Dennis Hedegaard <dennis@dhedegaard.dk>
2016-08-12 19:32:56 +0200

    Converted urls view strings to methods.

--- urls/urls.py
+++ urls/urls.py
@@ -1,15 +1,17 @@
 from __future__ import absolute_import
 
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
+from . import views
 
 urlpatterns = [
-    url(r'^create$', 'urls.views.create', name='create'),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
+    url(r'^create$', views.create, name='create'),
+    url(r'^accounts/login/$', auth_views.login,
         {'template_name': 'login.html'}, name='login'),
-    url(r'^logout$', 'urls.views.logout', name='logout'),
-    url(r'^$', 'urls.views.list', name='list'),
-    url(r'^(?P<keyword>.+)/delete/$', 'urls.views.delete', name='delete'),
-    url(r'^(?P<keyword>.+)/edit/$', 'urls.views.create', name='edit'),
-    url(r'^(?P<keyword>.+)$', 'urls.views.redirector', name='redirector'),
+    url(r'^logout$', views.logout, name='logout'),
+    url(r'^$', views.list, name='list'),
+    url(r'^(?P<keyword>.+)/delete/$', views.delete, name='delete'),
+    url(r'^(?P<keyword>.+)/edit/$', views.create, name='edit'),
+    url(r'^(?P<keyword>.+)$', views.redirector, name='redirector'),
 ]
