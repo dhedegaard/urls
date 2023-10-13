ed501a6fd07bd38b21f6f702a7c76d156123fc8f
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-10-13 17:29:31 +0200

    formatting and some type hints

--- urls/urls.py
+++ urls/urls.py
@@ -2,15 +2,20 @@ from __future__ import absolute_import
 
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.urls import URLPattern
 
 from . import views
 
-urlpatterns = [
-    url(r'^create$', views.create, name='create'),
-    url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name='login.html'), name='urls_login'),
-    url(r'^logout$', auth_views.LogoutView.as_view(next_page='/'), name='urls_logout'),
-    url(r'^$', views.list, name='list'),
-    url(r'^(?P<keyword>.+)/delete/$', views.delete, name='delete'),
-    url(r'^(?P<keyword>.+)/edit/$', views.create, name='edit'),
-    url(r'^(?P<keyword>.+)$', views.redirector, name='redirector'),
+urlpatterns: list[URLPattern] = [
+    url(r"^create$", views.create, name="create"),
+    url(
+        r"^accounts/login/$",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="urls_login",
+    ),
+    url(r"^logout$", auth_views.LogoutView.as_view(next_page="/"), name="urls_logout"),
+    url(r"^$", views.list, name="list"),
+    url(r"^(?P<keyword>.+)/delete/$", views.delete, name="delete"),
+    url(r"^(?P<keyword>.+)/edit/$", views.create, name="edit"),
+    url(r"^(?P<keyword>.+)$", views.redirector, name="redirector"),
 ]
--- urls/views.py
+++ urls/views.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, unicode_literals
+from typing import Optional
 
 import requests
 from django.shortcuts import render, redirect, get_object_or_404
@@ -8,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.html import format_html
 from django.views.decorators.http import require_POST
 from django.http import (
+    HttpRequest,
     HttpResponse,
     HttpResponseServerError,
 )
@@ -17,67 +19,75 @@ from .forms import UrlForm
 from .models import Url
 
 
-def _add_event_message(request, keyword, event):
-    '''
+def _add_event_message(request: HttpRequest, keyword: str, event: str) -> None:
+    """
     This methods buids and adds a HTML string to django messages.
 
     :param request: The current request.
     :param keyword: The keyword causing the event, as a string.
     :param event: The name of the event, as a string (created/deleted/...).
-    '''
-    messages.success(request, format_html(
-        'The keyword <b><a href="{0}" target="_blank">{1}</a>'
-        '</b> has been <b>{2}</b> succesfully!',
-        reverse('redirector', args=(keyword,)), keyword, event))
+    """
+    messages.success(
+        request,
+        format_html(
+            'The keyword <b><a href="{0}" target="_blank">{1}</a>'
+            "</b> has been <b>{2}</b> succesfully!",
+            reverse("redirector", args=(keyword,)),
+            keyword,
+            event,
+        ),
+    )
 
 
-def _get_client_ip(request):
-    '''
+def _get_client_ip(request: HttpRequest):
+    """
     Returns the client IP address from a request.UrlForm
 
     :param request: A django response object.
     :returns: IP of the client of the the request, as a string.
-    '''
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    """
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
     if x_forwarded_for:
-        return x_forwarded_for.split(',')[0]
+        return x_forwarded_for.split(",")[0]
     else:
-        return request.META.get('REMOTE_ADDR')
+        return request.META.get("REMOTE_ADDR")
 
 
-def list(request):
+def list(request: HttpRequest) -> HttpResponse:
     urls = Url.objects.select_related().all()
 
     if not request.user.is_authenticated:
         urls = urls.filter(public=True)
 
-    return render(request, 'list.html', {
-        'urls': urls,
-        'title': 'List',
-        'changed_keyword': request.GET.get('changed_keyword'),
-        'changed_event_name': request.GET.get('changed_event_name'),
-    })
+    return render(
+        request,
+        "list.html",
+        {
+            "urls": urls,
+            "title": "List",
+            "changed_keyword": request.GET.get("changed_keyword"),
+            "changed_event_name": request.GET.get("changed_event_name"),
+        },
+    )
 
 
 @login_required
 @require_POST
 @transaction.atomic
-def delete(request, keyword):
+def delete(request: HttpRequest, keyword: str) -> HttpResponse:
     url = get_object_or_404(Url, keyword=keyword)
     url.delete()
-    _add_event_message(request, keyword, 'deleted')
-    return redirect('list')
+    _add_event_message(request, keyword, "deleted")
+    return redirect("list")
 
 
 @login_required
 @transaction.atomic
-def create(request, keyword=None):
-    if request.method == 'POST':
+def create(request: HttpRequest, keyword: Optional[str] = None):
+    if request.method == "POST":
         if keyword is not None:
             # If we're editing, make sure to provide the existing instance.
-            form = UrlForm(
-                request.POST,
-                instance=Url.objects.get(keyword=keyword))
+            form = UrlForm(request.POST, instance=Url.objects.get(keyword=keyword))
         else:
             # Save a new form.
             form = UrlForm(request.POST)
@@ -88,37 +98,42 @@ def create(request, keyword=None):
 
             # If we're changing the keyword of an existing redirect, delete the
             # old one.
-            if keyword and keyword != form.cleaned_data['keyword']:
+            if keyword and keyword != form.cleaned_data["keyword"]:
                 Url.objects.filter(keyword=keyword).delete()
 
             _add_event_message(
-                request, url.keyword,
-                'created' if keyword is None else 'changed')
-            return redirect('list')
+                request, url.keyword, "created" if keyword is None else "changed"
+            )
+            return redirect("list")
     else:
         if keyword is not None:
             form = UrlForm(instance=Url.objects.get(keyword=keyword))
         else:
             form = UrlForm()
-    return render(request, 'create.html', {
-        'form': form,
-        'redirect_count': Url.objects.all().count(),
-        'title': request.resolver_match.url_name.title(),
-        'keyword': keyword,
-    })
+    return render(
+        request,
+        "create.html",
+        {
+            "form": form,
+            "redirect_count": Url.objects.all().count(),
+            "title": request.resolver_match.url_name.title(),
+            "keyword": keyword,
+        },
+    )
 
 
-def _redirect_proxy(url):
+def _redirect_proxy(url: str) -> HttpResponse:
     try:
-        r = requests.get(url)
+        response: requests.Response = requests.get(url)
     except requests.exceptions.ConnectionError as e:
         return HttpResponseServerError(str(e))
     return HttpResponse(
-        r.text, content_type=r.headers.get('Content-Type', 'text/plain'))
+        response.text, content_type=response.headers.get("Content-Type", "text/plain")
+    )
 
 
-def redirector(request, keyword):
-    url = get_object_or_404(Url, keyword=keyword)
+def redirector(_request: HttpRequest, keyword: Optional[str]) -> HttpResponse:
+    url: Url = get_object_or_404(Url, keyword=keyword)
     if url.proxy:
         return _redirect_proxy(url.url)
     else:
