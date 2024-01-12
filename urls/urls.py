13031e294fe8e7bb607a77c1108ba1cf36a8673b
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-01-12 22:10:23 +0100

    Fix deprecation warnings

--- urls/urls.py
+++ urls/urls.py
@@ -1,21 +1,22 @@
 from __future__ import absolute_import
 
-from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from django.urls import URLPattern
+from django.urls import URLPattern, re_path
 
 from . import views
 
 urlpatterns: list[URLPattern] = [
-    url(r"^create$", views.create, name="create"),
-    url(
+    re_path(r"^create$", views.create, name="create"),
+    re_path(
         r"^accounts/login/$",
         auth_views.LoginView.as_view(template_name="login.html"),
         name="urls_login",
     ),
-    url(r"^logout$", auth_views.LogoutView.as_view(next_page="/"), name="urls_logout"),
-    url(r"^$", views.list, name="list"),
-    url(r"^(?P<keyword>.+)/delete/$", views.delete, name="delete"),
-    url(r"^(?P<keyword>.+)/edit/$", views.create, name="edit"),
-    url(r"^(?P<keyword>.+)$", views.redirector, name="redirector"),
+    re_path(
+        r"^logout$", auth_views.LogoutView.as_view(next_page="/"), name="urls_logout"
+    ),
+    re_path(r"^$", views.list, name="list"),
+    re_path(r"^(?P<keyword>.+)/delete/$", views.delete, name="delete"),
+    re_path(r"^(?P<keyword>.+)/edit/$", views.create, name="edit"),
+    re_path(r"^(?P<keyword>.+)$", views.redirector, name="redirector"),
 ]
