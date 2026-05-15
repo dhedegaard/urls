e76e74bbd2568efca8e498064285f490042b3a28
Dennis Hedegaard <dennis@dhedegaard.dk>
2026-05-15 18:04:59 +0200

    removed MANAGERS, removed SITE_ID and sites app, cleanup unused stuff

--- urls/settings.py
+++ urls/settings.py
@@ -1,5 +1,3 @@
-# Django settings for urls project.
-
 from __future__ import annotations
 import os
 from typing import Dict, cast
@@ -9,8 +7,6 @@ DEBUG: bool = "PRODUCTION" not in os.environ
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ADMINS = (("Dennis Hedegaard", "dennis@dhedegaard.dk"),)
-
-MANAGERS = ADMINS
 
 DATABASES = {
     "default": {
@@ -22,21 +18,18 @@ DATABASES = {
     }
 }
 
-# If there's a DATABASE_URL environ variable, expect it to be a postgres URL.
 if "DATABASE_URL" in os.environ and os.environ["DATABASE_URL"]:
     import dj_database_url
 
     DATABASES["default"] = cast(
         Dict[str, str],
-        dj_database_url.config(  # type: ignore
+        dj_database_url.config(
             default=os.environ["DATABASE_URL"],
             conn_max_age=600,
             ssl_require=True,
         ),
     )
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS: list[str] = (
     os.environ["ALLOWED_HOSTS"].split(",")
     if "ALLOWED_HOSTS" in os.environ
@@ -50,59 +43,19 @@ ALLOWED_HOSTS: list[str] = (
     ]
 )
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = "Europe/Copenhagen"
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = "en-us"
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ""
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ""
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
 STATIC_ROOT = os.path.join(ROOT, "staticfiles/")
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = "/static/"
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 )
 
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = "jw-_tia*(q^e^pb^5n7q492sxpm%k*xg!0ya7uuh$%htpd7*7g"
 
 MIDDLEWARE = (
@@ -115,25 +68,17 @@ MIDDLEWARE = (
 )
 
 ROOT_URLCONF = "urls.urls"
-
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = "urls.wsgi.application"
 
 INSTALLED_APPS = (
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
-    "django.contrib.sites",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-"urls",
+    "urls",
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
