a0b1105974c44c399bf643465f57c98c6e97e86f
Dennis Hedegaard <dennis@dhedegaard.dk>
2026-05-15 18:00:39 +0200

    don't use regex paths for paths that aren't regex-y

--- urls/urls.py
+++ urls/urls.py
@@ -1,19 +1,13 @@
 from django.contrib.auth import views as auth_views
-from django.urls import URLPattern, re_path
+from django.urls import URLPattern, path, re_path
 
 from . import views
 
 urlpatterns: list[URLPattern] = [
-    re_path(r"^create$", views.create, name="create"),
-    re_path(
-        r"^accounts/login/$",
-        auth_views.LoginView.as_view(template_name="login.html"),
-        name="urls_login",
-    ),
-    re_path(
-        r"^logout$", auth_views.LogoutView.as_view(next_page="/"), name="urls_logout"
-    ),
-    re_path(r"^$", views.list, name="list"),
+    path("create", views.create, name="create"),
+    path("accounts/login/", auth_views.LoginView.as_view(template_name="login.html"), name="urls_login"),
+    path("logout", auth_views.LogoutView.as_view(next_page="/"), name="urls_logout"),
+    path("", views.list, name="list"),
     re_path(r"^(?P<keyword>.+)/delete/$", views.delete, name="delete"),
     re_path(r"^(?P<keyword>.+)/edit/$", views.create, name="edit"),
     re_path(r"^(?P<keyword>.+)$", views.redirector, name="redirector"),
